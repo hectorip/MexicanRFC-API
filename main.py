7d72a9ea469d63e3a2da5176260cbd72cb7c2fb2
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-09-25 00:00:19 -0500

    heredocs

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import hug
+"""
+Generates the  RFC and CURP strings from the personal data provided.
+"""
+
 
 @hug.get()
 def rfc(name):
     return {"RFC": name[1].upper()}
 
 
-@hug.get()
+@hug.post()
 def curp(body):
+    """
+    Returns the CURP almost complete
+    """
     return body
